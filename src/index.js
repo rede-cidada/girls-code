0ca789af494c61ee41e4c6d46eab9fd5d4561a69
Ana julia <anajulia.anaju7@gmail.com>
2020-09-23 16:34:40 -0300

    About atualizado

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route></Route>
+        </Switch>
+    </BrowserRouter>
+)
