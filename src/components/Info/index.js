866b07fe2061cdc5044d5c6e3181ed447c82b7d6
anabeatriiz <anabeatrizvianaa@gmail.com>
2020-09-24 21:09:15 -0300

    atualizacao no footer

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import '../Info/style.css';
+import superWoman from "../../assets/super_woman.svg"
 
 export const Info = () => {
     return(
         <section className="info">
-            <h1 className="title-info">Girls code</h1>
-            <p className="paragraph-info">Nosso objetivo é te trazer para o mundo de TI mulher, topa? Então vem com a gente!</p>
+            <img className="image-superWoman" src={superWoman}/>
+
+            <div className="teste">
+                <h1 className="title-info">Girls code</h1>
+                <p className="paragraph-info">Nosso objetivo é te trazer para o mundo de TI mulher, topa? Então vem com a gente!</p>
+            </div>
         </section>
     );
 }
