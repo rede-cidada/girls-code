787f6c47916e3c099f5ab0d2a23dcbccd5c89870
Alice Cavalcanti <alicecavalcanti27@gmail.com>
2020-09-30 21:11:05 -0300

    segunda page do guia

--- src/Pages/Guia/index.js
+++ src/Pages/Guia/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageOne from '../../components/PageOne';
+import InternetPage from '../../components/InternetPage';
 
 const Guia = () => {
     return (
         <div className="PageOne-Guia">
             <PageOne />
+            <InternetPage />
         </div>
 
     )
---
+++ src/components/InternetPage/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.css";
+
+const InternetPage = () => {
+  return (
+    <div className="internet-area">
+      <section className="internet-texto">
+        <h2>Mas vamos lá, o que é a internet?</h2>
+        <p>Essa playlist rapidinha aqui vai te deixar a pá dos conceitos, olha só:</p>       
+      </section>
+
+      <section className="internet-video">
+        <iframe 
+        width="100%" 
+        height="300" 
+        src="https://www.youtube.com/embed/HNQD0qJ0TC4?list=PLQq8-9yVHyOYMFAc9v7Yb_cqmNMksEdrk" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+
+        </iframe>
+
+        <p>Entender isso é só um dos primeiros passos pra quem quer entender mais desse mundão que á a área de tecnologia. Mas não se afobe não, viu?</p>
+      </section>
+    </div>
+  );
+};
+
+export default InternetPage;
