14d0297974f16dab9ee19a49be9ae01d21909988
Ana julia <anajulia.anaju7@gmail.com>
2020-10-02 12:59:14 -0300

    pagina de colaboradores criada

--- src/Pages/Colaboradores/index.js
+++ src/Pages/Colaboradores/index.js
@@ -1,25 +1,40 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import './style.css'
 
 const Colaboradores = () => {
     const [dados, setDados] = useState([]);
 
     useEffect(() => {
         const getData = async () => {
-            const mulheres = await fetch('https://randomuser.me/api/?results=50&gender=female');
-            const sla = await mulheres.json();
+            const dataResponse = await axios.get(
+                "https://randomuser.me/api/?results=15&gender=female"
+            );
 
-            setDados(sla);
+            const data = await dataResponse.data.results;
+            setDados(data);
+        };
 
-        }
-
+        getData();
     }, []);
-
 
     return (
         <div className="colaboradores">
-            {dados.map(({ gender, name, }) =>
-                <section key={gender}>
-                </section>
+            <h1>Colaboradores</h1>
+            {dados.map((item, index) =>
+                <div className="card" key={index}>
+                    <div className="info">
+                        <img src={item.picture.large} alt={`${item.name.first} ${item.name.last}`} />
+                        <p><strong>{`${item.name.first} ${item.name.last}`}</strong></p>
+                    </div>
+                    <div className="contatos">
+                        <p>{item.email}</p>
+                        <p>{item.phone}</p>
+                        <button>Entrar em contato</button>
+                    </div>
+
+                </div>
             )}
         </div>
     )
