4724257e6e0ac34b7263631ee50e6f61227d51a4
Alice Cavalcanti <alicecavalcanti27@gmail.com>
2020-10-03 12:55:38 -0300

    css page colaboradores

--- src/Pages/Colaboradores/index.js
+++ src/Pages/Colaboradores/index.js
@@ -1,43 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import './style.css'
+import "./styles.css";
 
 const Colaboradores = () => {
-    const [dados, setDados] = useState([]);
+  const [dados, setDados] = useState([]);
 
-    useEffect(() => {
-        const getData = async () => {
-            const dataResponse = await axios.get(
-                "https://randomuser.me/api/?results=15&gender=female"
-            );
+  useEffect(() => {
+    const getData = async () => {
+      const dataResponse = await axios.get(
+        "https://randomuser.me/api/?results=8&gender=female"
+      );
 
-            const data = await dataResponse.data.results;
-            setDados(data);
-        };
+      const data = await dataResponse.data.results;
+      setDados(data);
+    };
 
-        getData();
-    }, []);
+    getData();
+  }, []);
 
-    return (
-        <div className="colaboradores">
-            <h1>Colaboradores</h1>
-            {dados.map((item, index) =>
+  return (
+    <div className="colaboradores">
+        
+      <div className="container">
+            <div className="colaboradores-text">
+                <h1>Mentoras</h1>
+                <p>O Girls code conta com um time de mentoras voluntárias dispostas a te ajudar nessa nova jornada!</p>
+                
+            </div>
+
+            <div className="colaboradores-main">
+
+                {dados.map((item, index) => (
                 <div className="card" key={index}>
                     <div className="info">
-                        <img src={item.picture.large} alt={`${item.name.first} ${item.name.last}`} />
-                        <p><strong>{`${item.name.first} ${item.name.last}`}</strong></p>
+                    <p className="colaboradores-nome">
+                        <strong>{`${item.name.first} ${item.name.last}`}</strong>
+                    </p>
+                    <p>Dev. Front End</p>
+                    <img
+                        src={item.picture.large}
+                        alt={`${item.name.first} ${item.name.last}`}
+                    />
                     </div>
-                    <div className="contatos">
-                        <p>{item.email}</p>
-                        <p>{item.phone}</p>
-                        <button>Entrar em contato</button>
-                    </div>
-
+                <div className="contatos">
+                    <p className="mail">{item.email}</p>
+                    <p>{item.phone}</p>
+                    <button>contato</button>
                 </div>
-            )}
-        </div>
-    )
+            </div>
+            ))}
+            </div>
+       
+      </div>
+    </div>
+  );
 };
 
 export default Colaboradores;
