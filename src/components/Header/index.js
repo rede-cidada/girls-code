1fee15948893cd47d5d48cbad75e9f857cc15204
Alice Cavalcanti <alicecavalcanti27@gmail.com>
2020-10-07 17:11:27 -0300

    teste menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./styles.css";
 
+
 import logoIcon from "../../assets/nova-logo.svg";
 
 import { Link } from 'react-router-dom';
 
+const menuAtv = (input) => {
+
+  if (input.checked === true) {
+
+    document.getElementById('navbar').disabled = true;
+  }
+  
+}
 
 const Header = () => {
   return (
+    
     <header className="header">
       <img src={logoIcon} alt="logo" className="logo-header" />
 
@@ -25,13 +35,13 @@ const Header = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/guia" className="nav-guia" onClick={() => input = true}>Guias</Link>
+            <Link to="/guia" className="nav-guia">Guias</Link>
           </li>
           <li>
             <Link to="/colaboradores">Colaboradores</Link>
        </li>
           <li>
-            <a href="#sobre">Sobre</a>
+            <a href="#sobre" onClick={menuAtv()}>Sobre</a>
           </li>
           <li>
             <a href="#footer">Contato</a>
@@ -41,5 +51,7 @@ const Header = () => {
     </header>
   );
 };
+
+
 
 export default Header;
