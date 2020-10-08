2163ed7661cf74606b9a37b5c377151d6af23e0b
Ana julia <anajulia.anaju7@gmail.com>
2020-10-07 21:24:50 -0300

    ajustadaindicações de cursos

--- src/components/Logica/index.js
+++ src/components/Logica/index.js
@@ -9,28 +9,34 @@ const Logica = () => {
       <section className="logica-area">
         <div className="logica-text">
           <h2>Logica de programação</h2>
-          <h3>Por que estudar Logica de programação?</h3>
-          <p>
-            É o primeiro passo porque isso não muda, independente da linguagem
-            que você escolher. Consiste em aprender técnicas para escrever
-            códigos que possam ser interpretados por computadores. A seguir tem
+          <div className="textos-sob">
+            <div>
+              <h3>Por que estudar Logica de programação?</h3>
+              <p>
+                É o primeiro passo porque isso não muda, independente da linguagem
+                que você escolher. Consiste em aprender técnicas para escrever
+                códigos que possam ser interpretados por computadores. A seguir tem
             alguns links onde to assistir e entender melhor.{" "}
-          </p>
+              </p>
 
-          <a
-            href="https://www.youtube.com/watch?v=JaTf3dhx464&t=5s"
-            target="_blanck"
-          >
-            {" "}
+              <a
+                href="https://www.youtube.com/watch?v=JaTf3dhx464&t=5s"
+                target="_blanck"
+              >
+                {" "}
             Click aqui para assistir{" "}
-          </a>
-          <h3>Mas o que é exatamente lógica da programação?</h3>
-          <p>
-            Lógica é a técnica utilizada para desenvolver instruções em uma
-            sequência para atingir determinado objetivo. É a organização e
-            planejamento de instruções, em um algoritmo, com o objetivo de
+              </a>
+            </div>
+            <div>
+              <h3>Mas o que é exatamente lógica da programação?</h3>
+              <p>
+                Lógica é a técnica utilizada para desenvolver instruções em uma
+                sequência para atingir determinado objetivo. É a organização e
+                planejamento de instruções, em um algoritmo, com o objetivo de
             tornar viável a implementação de um programa ou software.{" "}
-          </p>
+              </p>
+            </div>
+          </div>
           <h3>Algoritmo</h3>
           <p>
             O algoritmo é a própria sequência de instruções para a execução de
