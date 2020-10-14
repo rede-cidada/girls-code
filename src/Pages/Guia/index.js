32d200db1d4f5adabf3fae2a968b26e470524a42
Ana julia <anajulia.anaju7@gmail.com>
2020-10-13 23:18:51 -0300

    ajuste na parte de logica

--- src/Pages/Guia/index.js
+++ src/Pages/Guia/index.js
@@ -5,12 +5,12 @@ import InternetPage from '../../components/InternetPage';
 import ProfessionsPage from '../../components/ProfessionsPage';
 import AreasPage from '../../components/AreasPage';
 import Logica from '../../components/Logica';
-import Cursos from '../../components/Cursos';
+import Cursos from '../../components/cursos';
 
 
 const Guia = () => {
     useEffect(() => {
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
     }, []);
     return (
         <div className="PageOne-Guia">
